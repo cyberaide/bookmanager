ee70493ef5eecccf5cf0401d6a2a5e6c99acaf66
Gregor von Laszewski <laszewski@gmail.com>
2019-04-10 16:52:33 -0400

    find sources and headings from yaml

--- bookmanager/install/installer.py
+++ bookmanager/install/installer.py
@@ -26,6 +26,7 @@ from pathlib import Path
 from pprint import pprint
 from tabulate import tabulate
 from cloudmesh.management.configuration.config import Config
+from cloudmesh.common.FlatDict import FlatDict
 
 import oyaml as yaml
 import requests
@@ -159,6 +160,66 @@ def main():
             print(exc)
 
     pprint(d)
+
+    import collections
+
+
+    def process_image(link):
+        pass
+
+    def process_program(link):
+        pass
+
+    def process_dir(link):
+        pass
+
+
+    def find_sources(d,
+                     parent='',
+                     sep='/',
+                     sources=None,
+                     headings=None):
+
+        sources = []
+        headings = []
+
+        def _flatten(d, parent='', sep='/'):
+            if type(d) == list:
+                for entry in d:
+                    _flatten(entry, parent=parent)
+            elif type(d) == dict:
+                key = list(d.keys())[0]
+                child = d[key]
+                parent = f"{parent}{sep}{key}"
+                _flatten(child, parent=parent)
+                headings.append(parent)
+            else:
+                if d.startswith("i "):
+                    d=d.replace("i ","")
+                    process_image(d)
+                elif d.startswith("p "):
+                    d=d.replace("p ","")
+                    process_program(d)
+                elif d.startswith("r "):
+                    d=d.replace("r ","")
+                    process_dir(d)
+                sources.append(f"{parent}/{d}")
+
+        _flatten(d, parent='', sep='/')
+        return sources, headings
+
+    sources, headings = find_sources(d)
+
+    print("Sources")
+    print()
+    print('\n'.join(sources))
+    print()
+    print("HEADINGS")
+    print()
+    print('\n'.join(headings))
+    print()
+
+
 
     '''
     config = Config(config_path=arguments["YAML"])
