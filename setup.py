aaf028e23ab5cccfca35ba4e4be64476fcb3180d
Gregor von Laszewski <laszewski@gmail.com>
2019-04-10 17:10:49 -0400

    restructure code

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ setup(
     zip_safe=False,
     entry_points={
         'console_scripts': [
-            'bookmanager = bookmanager.install.installer:main',
+            'bookmanager = bookmanager.generator:main',
         ],
     }
 )
